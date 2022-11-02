6e9cd13fd965a59e9a4774f69477d17fa0e9f416
TEMP-ACC01 <117044990+TEMP-ACC01@users.noreply.github.com>
2022-11-03 04:41:07 +0530

    Delete server directory

--- server/db/connectio.js
+++
@@ -1,8 +0,0 @@
-const mongoose = require ('mongoose');
-
-
-const DB = process.env.DATABASE;// instea of passing link directly we are taking it from the .env file.
-
-mongoose.connect(DB).then(() => {
-    console.log('connection successful')
-}).catch(() => {console.log('error in connection')})
